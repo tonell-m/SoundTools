5136bb581f3cf0eccd889567739ff6d8ef47faa4
Marceau Tonelli <Marceau@Mac-mini-de-Archibald-Picq.local>
2016-10-03 16:14:13 +0200

    Creating XCode project

---
+++ SoundTools/SoundTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundTools
+//
+//  Created by Marceau Tonelli on 03/10/2016.
+//  Copyright © 2016 Marceau Tonelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
